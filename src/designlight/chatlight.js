7dbeadb8b2f91bc7223ec45f8f7fc9d6c39b567e
Vincent Edobor <edoborcosmas@gmail.com>
2024-09-02 16:08:19 +0100

    latest

--- src/designlight/chatlight.js
+++ src/designlight/chatlight.js
@@ -64,11 +64,17 @@ const Chatlight = ({ handleOut, userName, userEmail }) => {
     inputRef.current.value = '';
   };
 
-  const processBotReply = (response) => {
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
-    const processedResponse = response.replace(urlRegex, '<button class="link-button" data-url="$1">Click Here</button>');
-    return processedResponse;
-  };
+//  const processBotReply = (response) => {
+//   const urlRegex = /(https?:\/\/[^\s\)\.]+)/g;
+//   const processedResponse = response.replace(urlRegex, '<button class="link-button" data-url="$1">Click Here</button>');
+//   return processedResponse;
+// };
+
+function processBotReply(response) {
+  const urlRegex = /\[.*?\]\((https?:\/\/[^\s]+)\)/;
+  const processedResponse = response.replace(urlRegex, '<button class="link-button" data-url="$1">Click Here</button>');
+  return processedResponse;
+}
 
   useEffect(() => {
     const handleButtonClick = (event) => {
